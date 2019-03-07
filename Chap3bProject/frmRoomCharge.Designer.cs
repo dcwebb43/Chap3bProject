dcf2b743810fb8e6bdc4b4ef931323878c2bbb19
Dalyssa <Dalyssa@DESKTOP-QJUVPV9>
2019-03-07 15:56:03 -0600

    Add project files.

---
+++ Chap3bProject/frmRoomCharge.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Chap3bProject
+{
+    partial class frmRoomCharge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNightlyCharge = new System.Windows.Forms.TextBox();
+            this.txtNights = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMisc = new System.Windows.Forms.TextBox();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.txtRoomService = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblAdditionalCharges = new System.Windows.Forms.Label();
+            this.lblRoomCharges = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grpRoomInfo = new System.Windows.Forms.GroupBox();
+            this.grpAdditionalCharges = new System.Windows.Forms.GroupBox();
+            this.grpTotalCharges = new System.Windows.Forms.GroupBox();
+            this.btnShowGrandTotal = new System.Windows.Forms.Button();
+            this.grpRoomInfo.SuspendLayout();
+            this.grpAdditionalCharges.SuspendLayout();
+            this.grpTotalCharges.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Acme Hotel Room Charge Calculator";
+            // 
+            // lblDate
+            // 
+            this.lblDate.Location = new System.Drawing.Point(114, 72);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(196, 16);
+            this.lblDate.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Today\'s Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nightly Charge";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nights";
+            // 
+            // txtNightlyCharge
+            // 
+            this.txtNightlyCharge.Location = new System.Drawing.Point(93, 59);
+            this.txtNightlyCharge.Name = "txtNightlyCharge";
+            this.txtNightlyCharge.Size = new System.Drawing.Size(66, 20);
+            this.txtNightlyCharge.TabIndex = 1;
+            // 
+            // txtNights
+            // 
+            this.txtNights.Location = new System.Drawing.Point(93, 27);
+            this.txtNights.Name = "txtNights";
+            this.txtNights.Size = new System.Drawing.Size(66, 20);
+            this.txtNights.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Misc.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telephone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Room Service";
+            // 
+            // txtMisc
+            // 
+            this.txtMisc.Location = new System.Drawing.Point(91, 95);
+            this.txtMisc.Name = "txtMisc";
+            this.txtMisc.Size = new System.Drawing.Size(66, 20);
+            this.txtMisc.TabIndex = 2;
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(91, 60);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(66, 20);
+            this.txtTelephone.TabIndex = 1;
+            // 
+            // txtRoomService
+            // 
+            this.txtRoomService.Location = new System.Drawing.Point(91, 25);
+            this.txtRoomService.Name = "txtRoomService";
+            this.txtRoomService.Size = new System.Drawing.Size(66, 20);
+            this.txtRoomService.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(27, 154);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(73, 13);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "Total Charges";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(75, 123);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Tax";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(54, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(46, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Subtotal";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Additional Charges";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Room Charges";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.Location = new System.Drawing.Point(106, 153);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(87, 19);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblTax
+            // 
+            this.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTax.Location = new System.Drawing.Point(106, 122);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(87, 19);
+            this.lblTax.TabIndex = 3;
+            this.lblTax.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSubtotal.Location = new System.Drawing.Point(106, 91);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(87, 19);
+            this.lblSubtotal.TabIndex = 2;
+            this.lblSubtotal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblAdditionalCharges
+            // 
+            this.lblAdditionalCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAdditionalCharges.Location = new System.Drawing.Point(106, 60);
+            this.lblAdditionalCharges.Name = "lblAdditionalCharges";
+            this.lblAdditionalCharges.Size = new System.Drawing.Size(87, 19);
+            this.lblAdditionalCharges.TabIndex = 1;
+            this.lblAdditionalCharges.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblRoomCharges
+            // 
+            this.lblRoomCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRoomCharges.Location = new System.Drawing.Point(106, 29);
+            this.lblRoomCharges.Name = "lblRoomCharges";
+            this.lblRoomCharges.Size = new System.Drawing.Size(87, 19);
+            this.lblRoomCharges.TabIndex = 0;
+            this.lblRoomCharges.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(39, 485);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(82, 45);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "&Calculate Charges";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(187, 485);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(82, 45);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(335, 485);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(82, 45);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // grpRoomInfo
+            // 
+            this.grpRoomInfo.Controls.Add(this.label2);
+            this.grpRoomInfo.Controls.Add(this.txtNights);
+            this.grpRoomInfo.Controls.Add(this.txtNightlyCharge);
+            this.grpRoomInfo.Controls.Add(this.label4);
+            this.grpRoomInfo.Location = new System.Drawing.Point(47, 136);
+            this.grpRoomInfo.Name = "grpRoomInfo";
+            this.grpRoomInfo.Size = new System.Drawing.Size(170, 92);
+            this.grpRoomInfo.TabIndex = 0;
+            this.grpRoomInfo.TabStop = false;
+            this.grpRoomInfo.Text = "Room Information";
+            // 
+            // grpAdditionalCharges
+            // 
+            this.grpAdditionalCharges.Controls.Add(this.txtTelephone);
+            this.grpAdditionalCharges.Controls.Add(this.txtRoomService);
+            this.grpAdditionalCharges.Controls.Add(this.txtMisc);
+            this.grpAdditionalCharges.Controls.Add(this.label7);
+            this.grpAdditionalCharges.Controls.Add(this.label5);
+            this.grpAdditionalCharges.Controls.Add(this.label6);
+            this.grpAdditionalCharges.Location = new System.Drawing.Point(259, 136);
+            this.grpAdditionalCharges.Name = "grpAdditionalCharges";
+            this.grpAdditionalCharges.Size = new System.Drawing.Size(173, 124);
+            this.grpAdditionalCharges.TabIndex = 1;
+            this.grpAdditionalCharges.TabStop = false;
+            this.grpAdditionalCharges.Text = "Additional Charges";
+            // 
+            // grpTotalCharges
+            // 
+            this.grpTotalCharges.Controls.Add(this.label10);
+            this.grpTotalCharges.Controls.Add(this.lblRoomCharges);
+            this.grpTotalCharges.Controls.Add(this.lblAdditionalCharges);
+            this.grpTotalCharges.Controls.Add(this.label12);
+            this.grpTotalCharges.Controls.Add(this.lblSubtotal);
+            this.grpTotalCharges.Controls.Add(this.label11);
+            this.grpTotalCharges.Controls.Add(this.lblTax);
+            this.grpTotalCharges.Controls.Add(this.lblTotal);
+            this.grpTotalCharges.Controls.Add(this.label9);
+            this.grpTotalCharges.Controls.Add(this.label8);
+            this.grpTotalCharges.Location = new System.Drawing.Point(140, 282);
+            this.grpTotalCharges.Name = "grpTotalCharges";
+            this.grpTotalCharges.Size = new System.Drawing.Size(209, 183);
+            this.grpTotalCharges.TabIndex = 2;
+            this.grpTotalCharges.TabStop = false;
+            this.grpTotalCharges.Text = "Total Charges";
+            // 
+            // btnShowGrandTotal
+            // 
+            this.btnShowGrandTotal.Location = new System.Drawing.Point(394, 409);
+            this.btnShowGrandTotal.Name = "btnShowGrandTotal";
+            this.btnShowGrandTotal.Size = new System.Drawing.Size(82, 45);
+            this.btnShowGrandTotal.TabIndex = 8;
+            this.btnShowGrandTotal.Text = "Show Grand Total";
+            this.btnShowGrandTotal.UseVisualStyleBackColor = true;
+            this.btnShowGrandTotal.Click += new System.EventHandler(this.btnShowGrandTotal_Click);
+            // 
+            // frmRoomCharge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 542);
+            this.Controls.Add(this.btnShowGrandTotal);
+            this.Controls.Add(this.grpTotalCharges);
+            this.Controls.Add(this.grpAdditionalCharges);
+            this.Controls.Add(this.grpRoomInfo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.label1);
+            this.Name = "frmRoomCharge";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DS3850: Chapter 3b Project";
+            this.Load += new System.EventHandler(this.frmRoomCharge_Load);
+            this.grpRoomInfo.ResumeLayout(false);
+            this.grpRoomInfo.PerformLayout();
+            this.grpAdditionalCharges.ResumeLayout(false);
+            this.grpAdditionalCharges.PerformLayout();
+            this.grpTotalCharges.ResumeLayout(false);
+            this.grpTotalCharges.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNightlyCharge;
+        private System.Windows.Forms.TextBox txtNights;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMisc;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.TextBox txtRoomService;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblAdditionalCharges;
+        private System.Windows.Forms.Label lblRoomCharges;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox grpRoomInfo;
+        private System.Windows.Forms.GroupBox grpAdditionalCharges;
+        private System.Windows.Forms.GroupBox grpTotalCharges;
+        private System.Windows.Forms.Button btnShowGrandTotal;
+    }
+}
+
---
+++ Chap3bProject/frmRoomCharge.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chap3bProject
+{
+    public partial class frmRoomCharge : Form
+    {
+
+        //Declare fields here...
+        private decimal fDecGrandTotal;
+
+        public frmRoomCharge()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRoomCharge_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToLongDateString();         
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // declare variables and constants...
+                int intNights;
+                decimal decNightlyCharge, decRoomService, decPhone, decMisc,
+                    decRoomCharges, decAddCharges, decSubtotal, decTax, decTotal;
+                const decimal decTaxRate=.09m;
+                // convert textbox values into numeric...
+                intNights = int.Parse(txtNights.Text);
+                decNightlyCharge = decimal.Parse(txtNightlyCharge.Text);
+                decRoomService = decimal.Parse(txtRoomService.Text);
+                decPhone = decimal.Parse(txtTelephone.Text);
+                decMisc = decimal.Parse(txtMisc.Text);
+                // perform calculations
+                decRoomCharges = intNights * decNightlyCharge;
+                decAddCharges = decRoomService + decPhone + decMisc;
+                decSubtotal = decRoomCharges + decAddCharges;
+                decTax = decSubtotal * decTaxRate;
+                decTotal = decSubtotal + decTax;
+                // increment summary field variable for grand total
+                //fDecGrandTotal = fDecGrandTotal + decTotal;
+                fDecGrandTotal += decTotal;
+                // display output...
+                lblRoomCharges.Text = decRoomCharges.ToString("n");
+                lblAdditionalCharges.Text = decAddCharges.ToString("n");
+                lblSubtotal.Text = decSubtotal.ToString("n");
+                lblTax.Text = decTax.ToString("n");
+                lblTotal.Text = decTotal.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem during calculations." + "\n" + ex.Message);
+            }
+        }
+
+        private void btnShowGrandTotal_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Grand Total Charges = " + fDecGrandTotal.ToString("c"));
+        }
+    }
+}
